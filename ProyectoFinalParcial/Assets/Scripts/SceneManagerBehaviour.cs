8f06fae8a56a9cd8ef807430a1ac8db8aefca1dc
MoyanoValentin <uzumakiivalen@hotmail.com>
2022-10-29 13:10:24 -0300

    Agregando escenas y el orden de la configuracion, tambien el script de cambio de escena

---
+++ ProyectoFinalParcial/Assets/Scripts/SceneManagerBehaviour.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerBehaviour : MonoBehaviour
+{
+    //Metodo de cambio de Escena asignado a un boton 
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
