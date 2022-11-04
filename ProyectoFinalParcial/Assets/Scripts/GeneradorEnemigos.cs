1e084af610f5f94444999cf99cfb0c7e6cfd6ad8
Gryuker <eskeleto@1958.outlook.es>
2022-11-04 03:19:30 -0300

    Agustin: Creacion de Generadores de enemigos

--- ProyectoFinalParcial/Assets/Scripts/GeneradorEnemigos.cs
+++ ProyectoFinalParcial/Assets/Scripts/GeneradorEnemigos.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GeneradorEnemigos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private GameObject enemigo;
+    [SerializeField]
+    private int tipoGenerador;
+    private int cont=0;
+
+    void Start(){
+        switch(tipoGenerador){
+        case 1:{
+            InvokeRepeating("GenerarEnemigo",0,10);
+            break;
+        }
+        case 2:{
+            InvokeRepeating("GenerarEnemigo",0,0);
+            break;
+        }
+      }    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void GenerarEnemigo(){
+        switch(tipoGenerador){
+           case 1:{
+            Instantiate(enemigo,transform.position,transform.rotation);
+            break;
+        }
+           case 2:{
+            if(cont<3){
+                Instantiate(enemigo,transform.position,transform.rotation);
+                cont++;
+            }
+            break;
+        }
     }
 }
+    void OnTriggerStay(Collider other){
+        if(other.gameObject.CompareTag("bala")){
+            Destroy(gameObject);
+        }
+      }
+}
+
