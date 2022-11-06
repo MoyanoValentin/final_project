71d4ff4764bdd07c1b39a7438580e4e365b9e95f
MoyanoSantiagoValentin <uzumakiivalen@hotmail.com>
2022-11-06 04:36:36 -0300

    Configurando escena de victoria, escena de creditos, musica.

---
+++ ProyectoFinalParcial/Assets/Scripts/CreditosBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosBehaviour : MonoBehaviour
+{
+    
+    void Start()
+    {
+       Invoke("WaitToEnd",21);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        
+    }
+
+     public void WaitToEnd()
+     {
+        
+        SceneManager.LoadScene("MainMenu");
+
+     }
+}
